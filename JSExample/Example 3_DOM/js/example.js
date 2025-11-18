d5ee166fac7cde284eefea165b82adff70126fe1
Nevena Kicanovic <nevena@contactus.se>
2025-11-18 13:39:22 +0100

    Lektion 10 - DOM exempel - lägga till element i listan, start och slut

---
+++ JSExample/Example 3_DOM/js/example.js
@@ -0,0 +1,16 @@
+let list = document.getElementsByTagName('ul')[0];
+//Lägga till element till start av listan
+let newItemFirst = document.createElement('li');
+let newtextFirst = document.createTextNode('kale');
+newItemFirst.appendChild(newtextFirst);
+list.insertBefore(newItemFirst, list.firstChild);
+
+
+//Lägga till element till slut av listan
+let newItemLast = document.createElement('li');
+let newtextLast = document.createTextNode('cream');
+newItemLast.appendChild(newtextLast);
+list.appendChild(newItemLast);
+
+//Byta färg för alla li elements att använda .cool class
+//Lägga till räknare i header h2
