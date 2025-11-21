9f9fcd6e0297cb930db18c8326a630e5b9bebbad
Nevena Kicanovic <nevena@contactus.se>
2025-11-21 14:25:31 +0100

    Google Maps API via gmp-map taggen

--- JSExample/Example 7_BasicGoogleMapsAPI/js/google-map.js
+++ JSExample/Example 7_BasicGoogleMapsAPI/js/google-map.js
@@ -1,18 +1,7 @@
-function init() {
-    let mapOptions = {
-        center: new google.maps.LatLng(40.782710, -73.965310),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 10
-
-    };
-    let venueMap;
-    venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
-
-function loadScript() {
+async function loadScript() {
     let script = document.createElement('script'); //Skapar ett <script> element
-    script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init'; //Lägger till värde av attributet src till elementet
-    document.body.appendChild(script); //Lägger till elementet script som sista barn i body
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA6myHzS10YXdcazAFalmXvDkrYCp5cLc8&libraries=maps,marker&v=weekly'; //Lägger till värde av attributet src till elementet
+    document.head.appendChild(script); //Lägger till elementet script som sista barn i body
 }
 
 window.onload = loadScript; //onload anropar loadScript()
