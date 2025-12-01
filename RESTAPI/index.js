4388c9ba369c65067631c363e87e6f7b0344552e
Nevena Kicanovic <nevena@contactus.se>
2025-12-01 11:34:14 +0100

    RestAPI IPify med map

--- RESTAPI/index.js
+++ RESTAPI/index.js
@@ -19,9 +19,17 @@ async function search_Ip_Address(ip_address) {
   );
   const response = await request.json();
 
-  /* Update the UI on the page */
   const { location, ip, isp } = response;
+
+  /* Update the ui on the page */
   update_ui(ip, location.city, location.timezone, isp);
+
+  /* Update the map on the page */
+  /* first remove all map instances if any */
+  if (map !== undefined && map !== null) {
+    map.remove();
+  }
+  create_map(location.lat, location.lng, location.country, location.region);
 }
 
 /* update UI function */
@@ -38,3 +46,28 @@ function update_ui(ip_address, location, timezone, isp) {
   utc.textContent = 'UTC' + timezone;
   isprovider.textContent = isp;
 }
+
+/* create the map */
+let map;
+function create_map(lat, lng, country, region) {
+  map = L.map('map').setView([lat, lng], 14);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  //Add marker to the map
+  const my_icon = L.icon({
+    iconUrl: 'images/icon-location.svg',
+    iconSize: [50,60],
+    iconAnchor: [40, 94],
+    popupAnchor: [-3, -76],
+  })
+  L.marker([lat, lng], {icon: my_icon}).addTo(map)
+    .bindPopup(`${region}, ${country}`)
+    .openPopup();
+}
+
+// const defaultIp = "26.37.52.179";
+// search_Ip_Address(defaultIp)
